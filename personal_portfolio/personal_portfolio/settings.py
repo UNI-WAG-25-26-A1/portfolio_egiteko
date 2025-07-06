0e926a9ef34799acafe79d045b088fe375dd1512
Izaskun Kortabitarte <ikortabitarte@uni.eus>
2025-07-06 14:43:22 +0200

    settings aldaketak

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -118,8 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'proyectos/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #login con oidc
 LOGIN_URL = '/accounts/login/'
